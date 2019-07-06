2fb2aa39a44c15aea99a224eb5907ce9266914b4
Wang.Gang <glogger@gmail.com>
2019-07-06 17:59:32 +0800

    删除变量

--- src/wsteel/tp51unit/init_tp51.php
+++ src/wsteel/tp51unit/init_tp51.php
@@ -6,8 +6,5 @@
  */
 use think\Container;
 
-define('ROOT_PATH', '/../../../../../../');
-define('APP_PATH', __DIR__ . '/../../../../../../application/');
-define('EXTEND_PATH', __DIR__.'/../../../../../../extend/');
 require __DIR__ . '/../../../../../../thinkphp/base.php';
 Container::get('app')->initialize();
